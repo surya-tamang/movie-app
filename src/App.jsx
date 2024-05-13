c964dd3de6d4748500d904c17bb8d45dbde39624
evil-89789 <tngsurya055@gmail.com>
2024-05-13 11:18:46 +0545

    add: interactive pagination buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,21 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
   const [watchLaterList, setWatchLaterList] = useState([]);
+  const [page, setPage] = useState(1);
 
-  console.log(searchValue);
-
-  const apiUrl = "https://api.themoviedb.org/3/trending/all/day?api_key=2693b4642782876103cf44020cb0ce6f";
+  const apiUrl = `https://api.themoviedb.org/3/trending/all/day?api_key=2693b4642782876103cf44020cb0ce6f&page=${page}`;
   const searchUrl = `https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=${searchValue}`;
 
   useEffect(() => {
-    getMovies(apiUrl);
-  }, [])
+    const url = searchValue ? searchUrl : apiUrl;
+    getMovies(url);
+  }, [page, searchValue])
 
   async function getMovies(url) {
     try {
       const response = await fetch(url)
       const data = await response.json();
       const movies = data.results;
-      console.log(movies);
       setMovies(movies)
     } catch (error) {
       console.log(error)
@@ -31,6 +30,7 @@ export default function App() {
   }
 
   const searchMovie = () => {
+    setPage(1);
     if (searchValue != '') {
       getMovies(searchUrl);
     }
@@ -44,6 +44,16 @@ export default function App() {
     const updatedWatchLaterList = watchLaterList.filter((movie) => movie.id !== movieToRemove.id);
     setWatchLaterList(updatedWatchLaterList);
   };
+
+  const handleNextClick = () => {
+    setPage(prevPage => prevPage + 1);
+  }
+
+  const handlePrevClick = () => {
+    if (page > 1) {
+      setPage(prevPage => prevPage - 1);
+    }
+  }
   return (
     <>
 
@@ -66,7 +76,7 @@ export default function App() {
       {/* ============== Pagination container ============ */}
 
       <div className='w-full flex justify-center my-5'>
-        <Pagination />
+        <Pagination nextClick={handleNextClick} pageValue={page} prevClick={handlePrevClick} />
       </div>
 
 
